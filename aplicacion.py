f557187a9b79cdaab237913eb91c11763a788c53
Smith Ramos <smithseleni@gmail.com>
2024-05-13 16:19:23 -0500

    Parte 03

--- aplicacion.py
+++ aplicacion.py
@@ -5,6 +5,38 @@ leerClave = open("clave.txt", "rt", encoding='utf8')
 datosUsuario = leerUsuario.read()
 datosClave = leerClave.read()
 
+def agregar_personas():
+    archdni = open("dni.txt", "at")
+    contndni = input("indicar dni para agregar: ")
+    archdni.write("\n" + contndni)
+    archdni.close()
+    archjnombre = open("nombre.txt", "at")
+    contjnombre = input("indicar nombre para agregar: ")
+    archjnombre.write("\n" + contjnombre)
+    archjnombre.close()
+    archjapellido = open("apellido.txt", "at")
+    contjapellido= input("indicar apellido para agregar: ")
+    archjapellido.write("\n" + contjapellido)
+    archjapellido.close()
+    
+    
+
+def listar_personas():
+    archlistardni = open("dni.txt", "rt", encoding='utf8')
+    archlistarnombre = open("nombre.txt", "rt", encoding='utf8')
+    archlistarapellido = open("apellido.txt", "rt", encoding='utf8')
+    contlista = []
+    while True:
+        dni = archlistardni.readline().strip()
+        nombre = archlistarnombre.readline().strip()
+        apellido = archlistarapellido.readline().strip()
+        if not dni:  # Si se llega al final del archivo
+            break
+        contlista.append(f"{dni}\t{nombre}\t{apellido}")
+    archlistardni.close()
+    archlistarnombre.close()
+    archlistarapellido.close()
+    return contlista
 
 def login():
     intentos_restantes = 2
@@ -23,6 +55,11 @@ def login():
                 print("Has excedido el número máximo de intentos.")
                 exit()
 
+def salir():
+    print("Gracias por su visita")
+
+def error():
+    print("Opción incorrecta")
 
 def menu():
     print("*** Datos Persona ***")
@@ -30,5 +67,20 @@ def menu():
     print("2. Agregar Personas")
     print("3. Salir")
 
+def opcion():
+    opcion = int(input("Ingrese opción: "))
+    while True:
+        if opcion == 1:
+            contlista = listar_personas()
+            print("\n".join(contlista))  # Muestra el resultado de listar_personas()
+            break
+        elif opcion == 2:
+            agregar_personas()
+            break
+        elif opcion == 3:
+            break
+        else:
+            print("Opción incorrecta")
 
-login()    
+login()
+opcion()
